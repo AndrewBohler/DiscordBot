dc851e6439cda19f3760a23292a232eaec6aca78
AndrewBohler <drew.bohler@gmail.com>
2019-02-12 09:20:17 -0600

    Made google ImportError explicit in the terminal
    Now you get the GitHub address inside the terminal if the package is
    missing instead of a long traceback!

--- google_lib.py
+++ google_lib.py
@@ -1,5 +1,12 @@
+import sys
+
 # pip install git+https://github.com/abenassi/Google-Search-API
-from google import google
+try:
+    from google import google
+except ImportError:
+    sys.exit(
+        'Error: Google-Search-API missing\n'
+        'Do: pip install git+https://github.com/abenassi/Google-Search-API')
 
 
 def search_results(search_string: str, num_pages: int=1):
