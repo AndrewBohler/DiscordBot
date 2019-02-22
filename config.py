4aad4ffe5d94df6f46f989c6a685d739822248dd
AndrewBohler <drew.bohler@gmail.com>
2019-02-22 15:38:21 -0600

    Human readable error if not DISCORD_TOKEN set
    Comes with instructions for Linux and Windows!!!

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
+import sys
 
 
 TEST_CHANNEL_ID = '538573824271187999'
-TOKEN = os.environ['DISCORD_TOKEN']
-
+try:
+    TOKEN = os.environ['DISCORD_TOKEN']
+except KeyError:
+    sys.exit(
+        'KeyError: DISCORD_TOKEN environment variable not set\n'
+        'Linux: export DISCORD_TOKEN="Paste bot token instide quotes"\n'
+        'Windows: set DISCORD_TOKEN="Paste bot token instide quotes"')
